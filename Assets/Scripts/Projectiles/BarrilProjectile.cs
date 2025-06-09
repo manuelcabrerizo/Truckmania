b2c4408902fd7025c39cdc7f754a7b19468711cf
manuelcabrerizo <manuelcabrerizo5@gmai.com>
2025-06-09 16:01:53 -0300

    add explosion force to player

--- Assets/Scripts/Projectiles/BarrilProjectile.cs
+++ Assets/Scripts/Projectiles/BarrilProjectile.cs
@@ -75,6 +75,15 @@ public class BarrilProjectile : Projectile
         barrilRenderer.enabled = false;
         collision.enabled = false;
         body.isKinematic = true;
+
+        Collider[] colliders = Physics.OverlapSphere(body.position, 50.0f, playerMask);
+        if (colliders.Length > 0)
+        {
+            Rigidbody playerBody = colliders[0].GetComponent<Rigidbody>();
+            playerBody.AddExplosionForce(100, body.position, 75.0f, 75.0f, ForceMode.Impulse);
+        }
+            
+
         StartCoroutine(SendReleaseaEventAfterSeconds(explosionParticleSystem.main.duration));
     }
 
